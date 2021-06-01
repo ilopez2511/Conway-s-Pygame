50f66370684884cee395d41aa2f0e1f62f5e880d
ilopez2511 <85149602+ilopez2511@users.noreply.github.com>
2021-06-01 00:03:27 -0600

    Update gameOfLife.py

--- gameOfLife.py
+++ gameOfLife.py
@@ -9,29 +9,39 @@ WIN = pygame.display.set_mode((W,H))
 pygame.display.set_caption("Game of Life")
 FPS = 60
 rows,cols = W//size, H//size
-grid = np.random.randint(0,2,(rows,cols))
+grid = np.random.randint(0,5,(rows,cols))
 
 def fieldCopy():
     field = grid
     return field
 
 def neighbors(field,x,y):
-    neighbor = [(field[x-1,y-1]),(field[x-1,y]),(field[x-1,y+1]),(field[x,y-1]),(field[x,y+1]),
-                (field[x+1,y-1]),(field[x+1,y]),(field[x+1,y+1])]
+    neighbor = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y+1),
+                (x+1,y-1),(x+1,y),(x+1,y+1)]
     count = 0
     for i,j in neighbor:
         if i >= 0 and j >= 0:
             try:
+                if field[i,j] != 1:
+                    count += 0
                 count += field[i,j]
             except:
                 pass
     return count
 
 def rules(x,y,count):
-    if count == 2 or count == 3:
-        grid[x,y] == 1
+    if count == 3:
+        grid[x,y] = 1
     elif count < 2 or count > 3:
-        grid[x,y] == 0
+        grid[x,y] = 0
+
+def run():
+    nextGen = fieldCopy()
+    count = 0
+    for i in range(nextGen.shape[0]):
+        for j in range(nextGen.shape[1]):
+            count = neighbors(nextGen,i,j)
+            rules(i,j,count)
 
 def display():
     WIN.fill("black")
@@ -45,14 +55,6 @@ def display():
             else:
                 pygame.draw.rect(WIN, color, box)
     pygame.display.update()
-
-def run():
-    nextGen = fieldCopy()
-    count = 0
-    for i in range(nextGen.shape[0]):
-        for j in range(nextGen.shape[1]):
-            count = neighbors(grid,i,j)
-            rules(i,j,count)
 
 def main():
     clock = pygame.time.Clock()
@@ -69,3 +71,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
